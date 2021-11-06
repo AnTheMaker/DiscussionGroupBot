39266338fd13e47160b58406541a2f30d8bf6cff
An <18481195+AnTheMaker@users.noreply.github.com>
2021-11-06 12:29:38 +0100

    Update about text

--- discussiongroupbot.php
+++ discussiongroupbot.php
@@ -16,7 +16,8 @@ if($msg == '/start' || $msg == '/start@discussiongroupbot'){
   $txt = '_Hello, nice to meet you!_
 Make me admin in your group and I will automatically remove all forwarded posts from your linked channel so the group doesn\'t get filled with channel messages!
 
-*Bot dev:* @PartyGuy';
+*Bot dev:* @PartyGuy
+*Currently maintained by:* @FabianPastor';
 
   $out = ['method'=>'sendMessage', 'chat_id' =>$chatID, 'text'=>$txt, 'parse_mode'=>'Markdown', 'reply_to_message_id'=>$msg_id];
 
